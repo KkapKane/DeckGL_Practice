99669e1d971c0253875ed752d3ba2a9d7da00ed3
Kane <LisaxKane@gmail.com>
2023-03-01 22:21:42 -0600

    working dataset visualized

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import NATIONAL_PARKS_DATA from "../data.json";
+import covid_Data from '../src/assets/us_county.json'
+import {Map} from 'react-map-gl'
+import DeckGL from "@deck.gl/react/typed";
+import { GeoJsonLayer } from "@deck.gl/layers/typed";
+
+const MAPBOX_ACCESS_TOKEN =
+ "pk.eyJ1Ijoia2thcGthbmUiLCJhIjoiY2xlcWhrOHpnMDRnZjNycTZkYTc3anMxMiJ9.CAQOno2NIvAsyP5EgfySDA";
+const MAP_STYLE = "https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json";
+
+
+const INITIAL_VIEW_STATE = {
+  latitude: 39.8283,
+  longitude: -98.5795,
+  zoom: 3,
+  bearing: 0,
+  pitch: 30,
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const onClick = (info: any) => {
+    if(info.object) {
+      alert(info.object.properties.Name)
+    }
+  }
+
+
+let layers = [
+  new GeoJsonLayer({
+    id: "nationalParks",
+    data:  NATIONAL_PARKS_DATA,
+    filled: true,
+    pointRadiusMinPixels: 5,
+    pointRadiusScale: 2000,
+    getPointRadius: (f) => 5,
+    getFillColor: (data: any) =>
+      data.properties.Name.includes("National Park")
+        ? [86, 144, 58, 250]
+        : [0, 0, 0, 250],
+    pickable: true,
+    autoHighlight: true,
+    onClick,
+  }),
+];
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <DeckGL  initialViewState={INITIAL_VIEW_STATE} controller={true} layers={layers}>
+      <Map mapStyle={MAP_STYLE} mapboxAccessToken={MAPBOX_ACCESS_TOKEN} />
+    </DeckGL>
   )
 }
 
